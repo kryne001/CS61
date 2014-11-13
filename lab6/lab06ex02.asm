8a7c58e0a005f510a0c15563d62490bd6563c93d
Kyler Rynear <kryne001@well.cs.ucr.edu>
2014-11-12 19:01:54 -0800

    finished lab06, ex02

--- lab6/lab06ex02.asm
+++ lab6/lab06ex02.asm
@@ -12,11 +12,38 @@
 .orig x3000
 LEA R0, start_message
 PUTS
-
+GETC
+OUT
 ADD R2, R0, #0
 LD R3, counter
+LOOP
+	ADD R2, R2, R2
+	BRn ITS_ONE
+	BRzp END_ITS_ONE
+	ITS_ONE
+		ADD R4, R4, #1
+		ADD R3, R3, #-1
+		BRp LOOP
+		BRnz END_LOOP
+	END_ITS_ONE
+	ADD R3, R3, #-1
+	BRp LOOP
+	BRnz END_LOOP
+END_LOOP
+
+LEA R0, end_message
+PUTS
+LD R0, #0
+LD R2, converter
+ADD R4, R4, R2
+ADD R0, R4, #0
+OUT
+
+HALT
 ;-----------
 ;data
 ;-----------
-start_message					.STRINGZ					"Input a single character "
-counter							.FILL						#0
+start_message					.STRINGZ					"Input a single character: "
+counter							.FILL						#16
+end_message						.STRINGZ					"\nThe number of 1s is "
+converter						.FILL						#48

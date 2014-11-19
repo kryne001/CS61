b212a73eff7c6cf4903f37c3d35a20a85325e77a
Kyler Rynear <kryne001@well.cs.ucr.edu>
2014-11-19 12:19:31 -0800

    started lab 7

---
+++ lab6/lab07ex01.asm
@@ -0,0 +1,11 @@
+;-----------------------------------
+;Name: Kyler Rynear
+;Login: kryne001
+;Email address: kryne001@ucr.edu
+;Assignment: lab 06, exercise 03
+;Lab Section: <021 or 022>
+;TA: Bryan Marsh
+;
+;I hereby certify that I have not received assistance on this assignment, or used code, from ANY outside source other than thte instruction team.
+;-----------------------------------
+
